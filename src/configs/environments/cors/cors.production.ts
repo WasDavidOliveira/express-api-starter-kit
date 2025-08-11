e20711cd4b6fe4faa6bd28d2a1593c3de27eb234
David Oliveira <daviddenisson2014@gmail.com>
2025-08-11 13:29:43 -0300

    refactor(cors): atualizar configuração CORS para permitir todos os origens e adicionar novos cabeçalhos permitidos

--- src/configs/environments/cors/cors.production.ts
+++ src/configs/environments/cors/cors.production.ts
@@ -1,10 +1,16 @@
 import { CorsOptions } from 'cors';
 
 export const productionCorsConfig: CorsOptions = {
-  origin: ['https://nosso-futuro.com.br'],
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'X-Requested-With',
+    'Accept',
+    'Origin',
+  ],
   maxAge: 86400,
   optionsSuccessStatus: 204,
 };
