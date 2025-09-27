8e3ab9715b7ec73ab7e76a8a0319e2f0e3303a2a
David Oliveira <daviddenisson2014@gmail.com>
2025-09-27 15:16:39 -0300

    refactor(routes): reorganizar rotas em um novo módulo para melhor estruturação
    - Criar um novo arquivo de rotas para agrupar as rotas de autenticação, permissões e funções relacionadas, melhorando a organização do código.
    - Atualizar o arquivo de rotas principal para utilizar o novo módulo, simplificando a estrutura de rotas da aplicação.

--- src/routes/v1/v1.routes.ts
+++ src/routes/v1/v1.routes.ts
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import authRoutes from '@/routes/v1/modules/auth/auth.routes';
-import permissionRoutes from '@/routes/v1/modules/permission/permission.routes';
-import { authMiddleware } from '@/middlewares/auth/auth.middlewares';
-import rolePermissionRoutes from '@/routes/v1/modules/role-permission/role-permission.routes';
-import roleRoutes from '@/routes/v1/modules/role/roles.routes';
+import modulesRoutes from '@/routes/v1/modules/modules.routes';
 
 const router: Router = Router();
 
-router.use('/auth', authRoutes);
-router.use('/permissions', authMiddleware, permissionRoutes);
-router.use('/roles', authMiddleware, roleRoutes);
-router.use('/roles-permissions', authMiddleware, rolePermissionRoutes);
+router.use('/', modulesRoutes);
 
 export default router;
